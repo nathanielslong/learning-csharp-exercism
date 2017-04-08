07ea85d98939d1bd3fcec9d2b08118ac5142d764
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-04-08 18:47:27 -0400

    finished leap, passing all tests

---
+++ leap/Leap.cs
@@ -0,0 +1,23 @@
+using System;
+
+public static class Leap
+{
+  public static bool IsLeapYear(int year)
+  {
+    if(year % 4 != 0)
+    {
+      return false;
+    }
+    else
+    {
+      if(year % 400 == 0 || year % 100 != 0)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+  }
+}
