e02f6d2ba58ea63bed56a5135f797abd0b7508bb
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-04-10 16:31:00 -0400

    got sum of multiples to work. secret was the break in the line, before higher numbers weren't working due to duplicates

---
+++ sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class SumOfMultiples
+{
+    public static int To(IEnumerable<int> multiples, int max)
+    {
+      int total = 0;
+
+      for(int i = 0; i < max; i++)
+      {
+        foreach(int multiple in multiples)
+        {
+          if(i % multiple == 0)
+          {
+            total += i;
+            break;
+          }
+        }
+      }
+      return total;
+    }
+}
