8002955f60a3af0855bcec63187139d5f7bed531
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-04-12 14:20:07 -0400

    all tests pass for pangram

---
+++ pangram/Pangram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public static class Pangram
+{
+  public static bool IsPangram(string input)
+  {
+    char[] alpha = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+    input = input.ToLower();
+    char[] inputArray = input.ToCharArray();
+
+    for(int i = 0; i < inputArray.Length; i++)
+    {
+      int index = Array.IndexOf(alpha, inputArray[i]);
+
+      if(index != -1)
+      {
+        alpha[index] = '\0';
+      }
+    }
+
+    foreach (char c in alpha)
+    {
+      if(!c.Equals('\0'))
+      {
+        return false;
+      }
+    }
+    return true;
+  }
+}
