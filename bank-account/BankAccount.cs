543e2662633da5ee120db564b6da3378a6d3605c
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-05-01 14:15:02 -0400

    first three tests pass

---
+++ bank-account/BankAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class BankAccount
+{
+  private bool closed = true;
+
+  public void Open()
+  {
+    closed = false;
+  }
+
+  public void Close()
+  {
+    closed = true;
+  }
+
+  public float Balance
+  {
+    get;
+    set;
+  }
+
+  public void UpdateBalance(float change)
+  {
+    Balance += change;
+  }
+}
